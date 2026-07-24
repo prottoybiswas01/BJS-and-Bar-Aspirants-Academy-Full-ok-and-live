4d191337439704671f8561c191a494ea259b5084
prottoybiswas01 <prottoybiswas575358@gmail.com>
2026-07-25 01:44:12 +0600

    61th

--- server/server.js
+++ server/server.js
@@ -55,9 +55,9 @@ const memoryDb = {
     enableAllMails: true
   },
   siteSettings: {
-    badgeText: "১৮তম BJS ও বার কাউন্সিল অ্যাডভোকেসি স্পেশাল ব্যাচে ভর্তি চলছে!",
-    heroTitle: "বিচারক ও আইনজীবী হওয়ার স্বপ্নে গড়ি নিশ্চিত সাফল্য",
-    heroSubtitle: "বাংলাদেশ জুডিশিয়াল সার্ভিস (BJS) এবং বার কাউন্সিল পরীক্ষায় শীর্ষস্থান অর্জনের জন্য দেশের সেরা বিচারক ও সুপ্রিম কোর্টের সিনিয়র আইনজীবীদের তত্ত্বাবধানে তৈরি পূর্ণাঙ্গ প্রস্তুতি কোর্স।",
+    badgeText: "",
+    heroTitle: "",
+    heroSubtitle: "",
     adminUsername: "prttoy",
     adminPassword: "ADMIN123@"
   }
